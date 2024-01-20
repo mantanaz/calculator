33f80c618de46dcba0c681e42ce07ccfcb5eb11f
Mantana <118508733+mantanaz@users.noreply.github.com>
2024-01-20 15:56:56 +0300

    script.js

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
-const formCalculatorEl = document.querySelector('#form-calc');
+const formCalculatorEl = document.querySelector('#form-calculator');
 const tipValue = document.querySelector("#tips-value");
 const totalValue = document.querySelector("#total-value");
-const inputBillValue = document.querySelector("#input-bill");
+const inputBillValue = document.querySelector("#input-bill-value");
 
 console.log(formCalculatorEl);
 
 const calcTips = (billValue, qualityService, qtyPeople) => {
-    const result = (billValue / qtyPeople) * (qualityService / 100);
-    console.log('result', result);
-}
+    const tipsPerPerson = (billValue / qtyPeople) * (qualityService / 100);
+};
+
+console.log('calcTips')
 
 const calcTotal = () => {};
 
 formCalculatorEl.addEventListener('submit', (e) => {
     e.preventDefault();
-
     const targetEl = e.target.elements;
-
     // console.log('test', inputBillValue.test.value);
-
     const inputBillvalue = targetEl['input-bill-value'].value;
     const selectQualityService = targetEl['select-quality-service'].value;
     const inputQtyPeople = targetEl['input-qty-people'].value;
@@ -28,5 +26,6 @@ formCalculatorEl.addEventListener('submit', (e) => {
     console.log('inputQtyPeople', inputQtyPeople);
 
     calcTips(inputBillvalue, selectQualityService, inputQtyPeople);
+    calcTotal();
 });
 
